60a6083382651206d1b6ec2a843cbe893ef9a210
jainam.bhavasar <jainam.bhavasar@uni.club>
2023-02-27 02:10:42 +0530

    Initial Commit

---
+++ clip-mac/clip_macApp.swift
@@ -0,0 +1,20 @@
+//
+//  clip_macApp.swift
+//  clip-mac
+//
+//  Created by Jainam Sunilkumar Bhavasar on 27/02/23.
+//
+
+import SwiftUI
+
+@main
+struct clip_macApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
